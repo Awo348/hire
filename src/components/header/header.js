69e85dfd167ffda68c6350835f9ba2e0801ef31a
temfa <akinfetemitope5@gmail.com>
2023-07-27 10:10:02 +0100

    DOne

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import { NavLink, useNavigate } from "react-router-dom";
+import Menu from "../menu/menu";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [click, setClick] = useState(true);
   return (
-    <div className="header-container">
-      <div className="header-wrapper">
-        <div className="header-nav-links">
-          <NavLink to="/vacancies">Vacanies</NavLink>
-          <NavLink to="/career">Career Advice</NavLink>
-          <NavLink to="/about">About us</NavLink>
-          <NavLink to="/advertise">Advertise</NavLink>
-        </div>
-        <div className="header-buttons">
-          <button>Login</button>
-          <button
-            onClick={() => {
-              navigate("/register");
-            }}>
-            Register
-          </button>
-        </div>
+    <>
+      <div className="header-menu">
+        <Menu
+          action={() => {
+            setClick(!click);
+          }}
+        />
       </div>
-    </div>
+      {click ? (
+        <div className="header-container">
+          <div className="header-wrapper">
+            <div className="header-nav-links">
+              <NavLink to="/vacancies">Vacanies</NavLink>
+              <NavLink to="/career">Career Advice</NavLink>
+              <NavLink to="/about">About us</NavLink>
+              <NavLink to="/advertise">Advertise</NavLink>
+            </div>
+            <div className="header-buttons">
+              <button>Login</button>
+              <button
+                onClick={() => {
+                  navigate("/register");
+                }}>
+                Register
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./menu.css";
 import Bars from "../../svg-components/bars/bars";
 
-const Menu = () => {
+const Menu = ({ action }) => {
   return (
     <div className="menu-container">
-      <div className="menu-wrapper">
+      <div className="menu-wrapper" onClick={action}>
         <Bars />
         <h2>Menu</h2>
       </div>
